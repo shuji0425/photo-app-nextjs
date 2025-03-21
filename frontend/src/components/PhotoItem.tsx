7936163a82462b5f077e035e7b6761c2e4a24a45
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-03-21 15:11:59 +0900

    スケルトン追加

--- frontend/src/components/PhotoItem.tsx
+++ frontend/src/components/PhotoItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 type PhotoProps = {
   photo: {
@@ -12,10 +13,14 @@ type PhotoProps = {
 };
 
 const PhotoItem = ({ photo, index }: PhotoProps) => {
+  const [isLoading, setIsLoading] = useState(true);
   const isPriority = index < 3;
 
   return (
-    <div className="shadow-md rounded-lg overflow-hidden">
+    <div className="shadow-md rounded-lg overflow-hidden relative">
+      {isLoading && (
+        <div className="absolute inset-0 bg-gray-300 animate-pulse z-0" />
+      )}
       <Image
         src={photo.url}
         alt={photo.title}
@@ -25,7 +30,10 @@ const PhotoItem = ({ photo, index }: PhotoProps) => {
         quality={50} // 画像の品質を調整
         priority={isPriority} // LCPの画像を優先ロード
         loading={isPriority ? "eager" : "lazy"}
-        className="w-full h-auto object-cover rounded-lg shadow-md"
+        className={`w-full h-auto object-cover rounded-lg shadow-md z-10 transition-opacity duration-700 ease-in-out ${
+          isLoading ? "opacity-20" : "opacity-100"
+        }`}
+        onLoad={() => setIsLoading(false)}
       />
       <div className="p-2 bg-white">
         <p className="text-sm text-center font-semibold mt-2">{photo.title}</p>
